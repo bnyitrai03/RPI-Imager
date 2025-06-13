027ccdb5553489f0a122887d2c40c2031135acb5
bnyitrai03 <bence.nyitrai@gmail.com.com>
2025-06-13 15:13:55 +0200

    Added config.txt

---
+++ pi-gen-custom-stage/01-config-chroot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+echo "dtparam=ant2" >> /boot/firmware/config.txt
+echo "Added dtparam=ant2 to /boot/firmware/config.txt"
+
+raspi-config nonint do_wifi_country HU
+echo "Set WiFi country to HU"
