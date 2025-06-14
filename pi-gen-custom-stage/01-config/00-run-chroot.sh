5be9bf1abca98cd35670d0906bef5a8f9fd459b8
bnyitrai03 <bence.nyitrai@gmail.com.com>
2025-06-14 18:22:56 +0200

    fixing wifi connection issue

--- pi-gen-custom-stage/01-config/00-run-chroot.sh
+++ pi-gen-custom-stage/01-config/00-run-chroot.sh
@@ -3,5 +3,30 @@
 echo "dtparam=ant2" >> /boot/firmware/config.txt
 echo "Added dtparam=ant2 to /boot/firmware/config.txt"
 
-# raspi-config nonint do_wifi_country HU
-# echo "Set WiFi country to HU"
+raspi-config nonint do_wifi_country HU
+echo "Set WiFi country to HU"
+
+# Configure WiFi using NetworkManager connection file
+tee "/etc/NetworkManager/system-connections/$WIFI_SSID.nmconnection" > /dev/null << EOF
+[connection]
+id=$WIFI_SSID
+uuid=$(uuidgen)
+type=wifi
+autoconnect=true
+
+[wifi]
+ssid=$WIFI_SSID
+
+[wifi-security]
+key-mgmt=wpa-psk
+psk=$WIFI_PASS
+
+[ipv4]
+method=auto
+
+[ipv6]
+method=auto
+EOF
+
+chmod 600 "/etc/NetworkManager/system-connections/$WIFI_SSID.nmconnection"
+echo "Created WiFi connection for $WIFI_SSID network"
