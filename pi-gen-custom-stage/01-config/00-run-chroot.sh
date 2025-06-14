b30ca4b6155bc70ac8789456a632a495323b41bb
bnyitrai03 <bence.nyitrai@gmail.com.com>
2025-06-14 14:09:39 +0200

    trying out package building into the image

--- pi-gen-custom-stage/01-config/00-run-chroot.sh
+++ pi-gen-custom-stage/01-config/00-run-chroot.sh
@@ -3,5 +3,5 @@
 echo "dtparam=ant2" >> /boot/firmware/config.txt
 echo "Added dtparam=ant2 to /boot/firmware/config.txt"
 
-raspi-config nonint do_wifi_country HU
-echo "Set WiFi country to HU"
+# raspi-config nonint do_wifi_country HU
+# echo "Set WiFi country to HU"
