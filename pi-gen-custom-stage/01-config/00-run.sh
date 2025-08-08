bf209468c799865fc10ce7d9e691306b37f2d7ef
bnyitrai03 <bence.nyitrai@gmail.com.com>
2025-08-08 11:37:05 +0200

    creating image with all services

--- pi-gen-custom-stage/01-config/00-run.sh
+++ pi-gen-custom-stage/01-config/00-run.sh
@@ -4,8 +4,13 @@ on_chroot << EOF
 echo "dtparam=ant2" >> /boot/firmware/config.txt
 echo "Enabled Antenna"
 
+echo "dtparam=i2c_arm=on" >> /boot/firmware/config.txt
+echo "Enabled I2C"
+
 raspi-config nonint do_wifi_country HU
 echo "Set WiFi country to HU"
+
+echo "Current user being configured: ${FIRST_USER_NAME}"
 EOF
 
 install -v -m 600 files/main.nmconnection ${ROOTFS_DIR}/etc/NetworkManager/system-connections/
@@ -17,9 +22,9 @@ EOF
 on_chroot << EOF
 mkdir -p /etc/nginx/ssl
 openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/rpicm5.key \
-  -out /etc/nginx/ssl/rpicm5.crt \
-  -subj "/C=HU/ST=Budapest/L=Budapest/O=SZTAKI/CN=rpicm5"
+  -keyout /etc/nginx/ssl/nicowl.key \
+  -out /etc/nginx/ssl/nicowl.crt \
+  -subj "/C=HU/ST=Budapest/L=Budapest/O=SZTAKI/CN=nicowl"
 EOF
 
 # Configure nginx
@@ -36,10 +41,14 @@ echo "v4l2loopback" | tee /etc/modules-load.d/v4l2loopback.conf
 echo 'options v4l2loopback devices=6 video_nr=10,11,12,14,15,16 card_label="Cam1,CamL1,CamR1,Cam2,CamL2,CamR2"' | tee /etc/modprobe.d/v4l2loopback-options.conf
 EOF
 
+# Add udev rule to enable sensor reading
+install -v -m 644 files/99-mcp.rules ${ROOTFS_DIR}/etc/udev/rules.d/
+
 # Install Services
 install -v -d ${ROOTFS_DIR}/opt/BabyMonitor
 cp -r files/Services/CameraManagerService ${ROOTFS_DIR}/opt/BabyMonitor/CameraManagerService
 cp -r files/Services/StreamingService ${ROOTFS_DIR}/opt/BabyMonitor/StreamingService
+cp -r files/Services/SensorService ${ROOTFS_DIR}/opt/BabyMonitor/SensorService
 
 # Create virtual environments and install dependencies
 on_chroot << EOF
@@ -56,6 +65,13 @@ source .stream_venv/bin/activate
 pip install -r requirements.txt
 deactivate
 echo "Installed StreamingService with dependencies"
+
+cd /opt/BabyMonitor/SensorService
+python3 -m venv .sensor_venv
+source .sensor_venv/bin/activate
+pip install -r requirements.txt
+deactivate
+echo "Installed SensorService with dependencies"
 
 chown -R "${FIRST_USER_NAME}:${FIRST_USER_NAME}" /opt/BabyMonitor
 EOF
@@ -98,10 +114,37 @@ WantedBy=multi-user.target
 
 EOF
 echo "StreamingService created"
+
+cat > ${ROOTFS_DIR}/etc/systemd/system/BabyMonitor-Sensor.service << EOF
+[Unit]
+Description=Baby Monitor Sensor Service
+After=network.target
+
+[Service]
+Type=simple
+User=${FIRST_USER_NAME}
+WorkingDirectory=/opt/BabyMonitor/SensorService
+ExecStart=/opt/BabyMonitor/SensorService/.sensor_venv/bin/python -m uvicorn src.sensor_api:app --host 127.0.0.1 --port 8001
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+echo "SensorService created"
+
+on_chroot << EOF
+systemctl enable BabyMonitor-CameraManager.service
+systemctl enable BabyMonitor-Streaming.service
+systemctl enable BabyMonitor-Sensor.service
+echo "Enabled systemd services"
+EOF
 echo "Image customization done"
 
-# on_chroot << EOF
-# systemctl enable BabyMonitor-CameraManager.service
-# systemctl enable BabyMonitor-Streaming.service
-# echo "Enabled systemd services"
-# EOF
+### ON THE PI ###
+# sudo systemctl start BabyMonitor-Streaming.service
+# sudo systemctl start BabyMonitor-CameraManager.service
+# sudo systemctl start BabyMonitor-Sensor.service
+# get the crt from: /etc/nginx/ssl/nicowl.crt
+###################
